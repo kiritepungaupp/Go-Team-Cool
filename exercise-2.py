ad693681999c1192c8eeb769669ac98a57c80be4
kiritepungaupp <156395646+kiritepungaupp@users.noreply.github.com>
2024-03-25 17:00:57 +0700

    finished 2 and 4

--- exercise-2.py
+++ exercise-2.py
@@ -1,2 +1,10 @@
 def index_power(numnbers, n):
-    ...
+    if len(numnbers) < n+1:
+        return -1
+    power = 1
+    for x in range(n):
+        power *= numnbers[n]
+    return power
+
+
+print(index_power([1,2,3,4,5,6],4))
--- exercise-4.py
+++ exercise-4.py
@@ -1,2 +1,25 @@
-def chunking_by(numbers, chunck):
-    ...
+def chunking_by(numbers, chunk):
+    counter = 0
+    final_list = []
+    tempo_list = []
+    if len(numbers)/chunk == round(len(numbers)/chunk):
+        itteration = round((len(numbers)/int(chunk)))
+    else:
+        itteration = round((len(numbers)/int(chunk)))+1
+    
+    for x in range(itteration):
+        tempo_list = []
+        for y in range(chunk):
+            try:
+                tempo_list.append(numbers[counter])
+                counter +=1
+            except:
+                continue
+        final_list.append(tempo_list)
+    final_final_list = []
+    for x in final_list:
+        if len(x) > 0:
+            final_final_list.append(x)
+    return final_final_list
+
+print(chunking_by([1,2,3,4,5],3))
