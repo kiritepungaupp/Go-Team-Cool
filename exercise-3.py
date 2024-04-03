49cd3a030876a4883228c307ea075772eb081a77
sothydarunny <156157314+sothydarunny@users.noreply.github.com>
2024-04-03 11:41:10 +0700

    Update exercise-3.py

--- exercise-3.py
+++ exercise-3.py
@@ -1,2 +1,12 @@
 def remove_all_after(numbers, n):
-    ...
+    if numbers and n in numbers:
+        index = numbers.index(n)
+        return numbers[max(index - 2, 0):index + 1]
+    else:
+        return numbers
+    
+numbers = [1, 2, 3, 4, 5]
+n = 3
+
+result = remove_all_after(numbers, n)
+print(result)
