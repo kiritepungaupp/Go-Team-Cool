95fb8c0d04149c75206e4db17567d2011b3ef295
kiritepungaupp <156395646+kiritepungaupp@users.noreply.github.com>
2024-04-07 23:06:46 +0700

    final

--- exercise-1.py
+++ exercise-1.py
@@ -1,2 +1,10 @@
 def replace_last(numbers):
-    ...
+    
+    if len(numbers) == 0:
+        return []
+    if len(numbers) == 1:
+        return numbers 
+    first_number = numbers.pop(0)
+    replace = numbers.append(first_number)
+    return numbers
+
--- exercise-5.py
+++ exercise-5.py
@@ -1,2 +1,21 @@
 def reverse_ascending(numbers):
-    ...
+    temp_list = [numbers[0]]
+    final_list = []
+    for x in range(1,len(numbers)):
+
+        if numbers[x] <= numbers[x-1]:
+            final_list.append(temp_list)
+            temp_list=[numbers[x]]
+            continue
+        temp_list.append(numbers[x])
+
+    final_list.append(temp_list)
+    finally_list = []
+    for x in final_list:
+        x.sort(reverse = True)
+        finally_list = finally_list + x
+
+    return finally_list
+
+
+print(reverse_ascending([1,2,3,4,2,1,2,3]))
